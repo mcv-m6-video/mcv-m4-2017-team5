24b04b82c36dd3c11102d43599d103c4c7f85be3
Roque <roque.rodriguez.outeiral@gmail.com>
2017-01-03 11:54:59 +0100

    two fixed scripts

--- task1Gray.m
+++ task1Gray.m
@@ -5,44 +5,57 @@ addpath(genpath('.'))
 
 %Directory where the masks of the different sets are placed
 
-directory_imagesIn = 'Database/highway/input/';
-directory_imagesGT = 'Database/highway/groundtruth/';
+directory_sequence = 'Database/Week02/highway/';
+% directory_sequence = 'Database/Week02/fall/';
+% directory_sequence = 'Database/Week02/traffic/';
+
+directory_imagesIn = strcat(directory_sequence, 'input/');
+directory_imagesGT = strcat(directory_sequence, 'groundtruth/');
 
 dirIn = dir([directory_imagesIn '/*.jpg']);
 dirGT = dir([directory_imagesGT '/*.png']);
+percentage = 0.5;
+alpha=0.07;
 
-images1 = zeros(240,320,100);
-images2 = zeros(240,320,100);
-imagesSeg = zeros(240,320,100);
-alpha=0.088;
+train = 1:floor(percentage*length(dirIn));
+test = floor(percentage*length(dirIn))+1:length(dirIn);
+
+[dim1, dim2, dim3] = size(imread(dirIn(1).name));
+
+
+images_train = zeros(dim1, dim2,length(train));
+images_test = zeros(dim1, dim2, length(test));
+imagesSeg = zeros(dim1, dim2, length(test));
+
+
 
 
 %For the first 50% of the images from the dataset, the background model is
 %computed.
 
-for i = 1:length(dirIn)/2
-    images1(:,:,i) = rgb2gray(im2double(imread(dirIn(i).name)));
+for i = train
+    images_train(:,:,i) = rgb2gray(im2double(imread(dirIn(i).name)));
 end
 
 
-mu = zeros(240,320);
-sigma = zeros(240,320);
+mu = zeros(dim1,dim2);
+sigma = zeros(dim1,dim2);
 
-for m = 1:240
-    for n = 1:320
+for m = 1:dim1
+    for n = 1:dim2
        
-            mu(m,n)=mean(images1(m,n,:));
-            sigma(m,n)=std(images1(m,n,:)); 
+            mu(m,n)=mean(images_train(m,n,:));
+            sigma(m,n)=std(images_train(m,n,:)); 
       
     end
 end
 
 %For the second 50% of the images from the dataset, the foreground is
 %segmented
-% for alpha = 0.001:0.001:0.2
-for i = length(dirIn)/2+1:length(dirIn)
-    images2(:,:,i-length(dirIn)/2) = rgb2gray(im2double(imread(dirIn(i).name)));
-    imagesSeg(:,:,i-length(dirIn)/2) = abs(images2(:,:,i-length(dirIn)/2)-mu(:,:)) >= alpha*(2+sigma(:,:));
+%  for alpha = 0.001:0.001:0.2
+for i = test
+    images_test(:,:,i-length(test)+1) = rgb2gray(im2double(imread(dirIn(i).name)));
+    imagesSeg(:,:,i-length(test)+1) = abs(images_test(:,:,i-length(test)+1)-mu(:,:)) >= alpha*(2+sigma(:,:));
        
 end
 
@@ -50,16 +63,16 @@ end
 
 %Evaluation step
 
-    TP_images = zeros(length(dirGT), 1);
-    FP_images = zeros(length(dirGT), 1);
-    FN_images = zeros(length(dirGT), 1);
-    TN_images = zeros(length(dirGT), 1);
+    TP_images = zeros(length(test), 1);
+    FP_images = zeros(length(test), 1);
+    FN_images = zeros(length(test), 1);
+    TN_images = zeros(length(test), 1);
  
 
- for i = length(dirIn)/2+1:length(dirIn)
+ for i = test
         
         imageGT =imread(strcat(directory_imagesGT,dirGT(i).name));
-        image = imagesSeg(:,:,i-length(dirIn)/2);
+        image = imagesSeg(:,:,i-length(test)+1);
         [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(image, imageGT);
         TP_images(i) = pixelTP;
         FP_images(i) = pixelFP;
@@ -72,10 +85,10 @@ end
     metrics2 = [pixelPrecision, pixelRecall,pixelFMeasure];
     
  
- %Uncomment this lines so an analysis in relation to alpha is performed (remember to change the for sentence too)   
+%  %Uncomment this lines so an analysis in relation to alpha is performed (remember to change the for sentence too)   
 %     metrics(:,uint8(alpha*1000)) = [pixelTP,pixelFP,pixelFN,pixelTN];
 %     metrics2(:,uint8(alpha*1000)) = [pixelPrecision, pixelRecall,pixelFMeasure];
-end
+% end
 % 
 % plot(alpha,metrics2(1,:),'r',alpha,metrics2(2,:),'g',alpha,metrics2(3,:),'b');
 % title 'Precision Recall Fmeasure'
--- task4w2RGB.m
+++ task4w2RGB.m
@@ -5,63 +5,74 @@ addpath(genpath('.'))
 
 %Directory where the masks of the different sets are placed
 
-directory_imagesIn = 'Database/highway/input/';
-directory_imagesGT = 'Database/highway/groundtruth/';
+directory_sequence = 'Database/Week02/highway/';
+% directory_sequence = 'Database/Week02/fall/';
+% directory_sequence = 'Database/Week02/traffic/';
+
+directory_imagesIn = strcat(directory_sequence, 'input/');
+directory_imagesGT = strcat(directory_sequence, 'groundtruth/');
 
 dirIn = dir([directory_imagesIn '/*.jpg']);
 dirGT = dir([directory_imagesGT '/*.png']);
+percentage = 0.5;
+alpha=0.088;
+ 
+train = 1:floor(percentage*length(dirIn));
+test = floor(percentage*length(dirIn))+1:length(dirIn);
 
-images1 = zeros(240,320,3,100);
-images2 = zeros(240,320,3,100);
-imagesSeg = zeros(240,320,100);
- alpha=0.088;
-%0.001 muy sobresegmentado. 0.01 sobresegmentado 0.05 bien, probar cerca.
+[dim1, dim2, dim3] = size(imread(dirIn(1).name));
+
+
+images_train = zeros(dim1, dim2, dim3,length(train));
+images_test = zeros(dim1, dim2, dim3, length(test));
+imagesSeg = zeros(dim1, dim2, length(test));
+
 
 %For the first 50% of the images from the dataset, the background model is
 %computed.
 
-for i = 1:length(dirIn)/2
-    images1(:,:,:,i) = im2double(imread(dirIn(i).name));
+for i = train
+    images_train(:,:,:,i) = im2double(imread(dirIn(i).name));
 end
 
 
-mu = zeros(240,320,3);
-sigma = zeros(240,320,3);
+mu = zeros(dim1, dim2, dim3);
+sigma = zeros(dim1, dim2, dim3);
 
-for m = 1:240
-    for n = 1:320
-        for k = 1:3
-            mu(m,n,k)=mean(images1(m,n,k,:));
-            sigma(m,n,k)=std(images1(m,n,k,:)); 
+for m = 1:dim1
+    for n = 1:dim2
+        for k = 1:dim3
+            mu(m,n,k)=mean(images_train(m,n,k,:));
+            sigma(m,n,k)=std(images_train(m,n,k,:)); 
         end
     end
 end
 
 %For the second 50% of the images from the dataset, the foreground is
 %segmented
-%for alpha = 0.001:0.001:0.2
-for i = length(dirIn)/2+1:length(dirIn)
-    images2(:,:,:,i-length(dirIn)/2) = im2double(imread(dirIn(i).name));
-    imagesSeg(:,:,i-length(dirIn)/2) = ...
-        abs(images2(:,:,1,i-length(dirIn)/2)-mu(:,:,1)) >= alpha*(2+sigma(:,:,1)) &...
-        abs(images2(:,:,2,i-length(dirIn)/2)-mu(:,:,2)) >= alpha*(2+sigma(:,:,2)) &...
-        abs(images2(:,:,3,i-length(dirIn)/2)-mu(:,:,3)) >= alpha*(2+sigma(:,:,3));
+for alpha = 0.001:0.001:0.2
+for i = test
+    images_test(:,:,:,i-length(test)+1) = im2double(imread(dirIn(i).name));
+    imagesSeg(:,:,i-length(test)+1) = ...
+        abs(images_test(:,:,1,i-length(test)+1)-mu(:,:,1)) >= alpha*(2+sigma(:,:,1)) &...
+        abs(images_test(:,:,2,i-length(test)+1)-mu(:,:,2)) >= alpha*(2+sigma(:,:,2)) &...
+        abs(images_test(:,:,3,i-length(test)+1)-mu(:,:,3)) >= alpha*(2+sigma(:,:,3));
 end
 
 %% 
 
 %Evaluation step
 
-    TP_images = zeros(length(dirGT), 1);
-    FP_images = zeros(length(dirGT), 1);
-    FN_images = zeros(length(dirGT), 1);
-    TN_images = zeros(length(dirGT), 1);
+    TP_images = zeros(length(test), 1);
+    FP_images = zeros(length(test), 1);
+    FN_images = zeros(length(test), 1);
+    TN_images = zeros(length(test), 1);
  
 
- for i = length(dirIn)/2+1:length(dirIn)
+ for i = test
         
         imageGT =imread(strcat(directory_imagesGT,dirGT(i).name));
-        image = imagesSeg(:,:,i-length(dirIn)/2);
+        image = imagesSeg(:,:,i-length(test)+1);
         [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(image, imageGT);
         TP_images(i) = pixelTP;
         FP_images(i) = pixelFP;
@@ -70,17 +81,17 @@ end
         
  end
     [pixelTP,pixelFP,pixelFN,pixelTN,pixelPrecision, pixelRecall,pixelFMeasure] = PerformanceEvaluationPixel(TP_images, FP_images, FN_images, TN_images);
-    metrics = [pixelTP,pixelFP,pixelFN,pixelTN];
-    metrics2 = [pixelPrecision, pixelRecall,pixelFMeasure];
-    
+%     metrics = [pixelTP,pixelFP,pixelFN,pixelTN];
+%     metrics2 = [pixelPrecision, pixelRecall,pixelFMeasure];
+%     
  
  %Uncomment this lines so an analysis in relation to alpha is performed (remember to change the for sentence too)   
-%     metrics(:,uint8(alpha*1000)) = [pixelTP,pixelFP,pixelFN,pixelTN];
-%     metrics2(:,uint8(alpha*1000)) = [pixelPrecision, pixelRecall,pixelFMeasure];
-%end
+    metrics(:,uint8(alpha*1000)) = [pixelTP,pixelFP,pixelFN,pixelTN];
+    metrics2(:,uint8(alpha*1000)) = [pixelPrecision, pixelRecall,pixelFMeasure];
+end
 
-% plot(alpha,metrics2(1,:),'r',alpha,metrics2(2,:),'g',alpha,metrics2(3,:),'b');
-% title 'Precision Recall Fmeasure'
-% xlabel('Alpha')
-% ylabel('Value')
-% legend('Precision','Recall','Fmeasure')
+plot(alpha,metrics2(1,:),'r',alpha,metrics2(2,:),'g',alpha,metrics2(3,:),'b');
+title 'Precision Recall Fmeasure'
+xlabel('Alpha')
+ylabel('Value')
+legend('Precision','Recall','Fmeasure')
