c9aa92098eb9704cffa6582e02222d30e3d07f45
Onofre Martorell <>
2017-01-03 12:35:35 +0100

    Grid search

--- Week 02/week2_task2_Gray.m
+++ Week 02/week2_task2_Gray.m
@@ -26,31 +26,50 @@ imagesSeg = recursive_gaussian( param );
 
 
 %%
-%Grid_search
-alphas = 0.001:0.005:0.2;
-rhos = 0:0.01:1;
-[Alpha, Rho] = meshgrid(alphas, rhos);
-[dim1, dim2] = size(Alpha);
+%Grid search
+min_alpha = 0.001;
+step_alpha = 0.05;
+max_alpha = 1;
+alphas = min_alpha:step_alpha:max_alpha;
+
+min_rho = 0;
+step_rho = 0.01;
+max_rho = 1;
+rhos = min_rho:step_rho:max_rho;
+
+
+dim1 = length(alphas);
+dim2 = length(rhos);
 
 directory_write = '../Results/week2/';
-alpha = 0;
-rho = 0;
 percentage = 0.5;
 
-param  = compute_parameters_w2(directory_sequence, directory_write, alpha, rho, percentage);
-metrics_search = zeros(1, 3, dim1*dim2);
-for i = 1:dim1*dim2
-    param.alpha = Alpha(i);
-    param.rho = Rho(i);
-    
-    %Computation_step
-    imagesSeg = recursive_gaussian( param );
-    
-    %Evaluation step
-    [ ~, metrics2 ] = evaluate_model_adaptive( imagesSeg, param );
-    metrics_search(:, :, i) = metrics2;
-end
+param  = compute_parameters_w2(directory_sequence, directory_write, 0, 0, percentage);
 
+% 3 matrices with metrics for each pair of values of alpha and rho
+%1: precision
+%2:recall
+%3:f-measure
+metrics_search = zeros(dim1, dim2, 3);
+for j = 1:dim2
+    for i = 1:dim1
+    
+        sub2ind([dim1 dim2], i, j)
+        param.alpha = alphas(i);
+        param.rho = rhos(j);
+    
+        %Computation_step
+        imagesSeg = recursive_gaussian( param );
+    
+        %Evaluation step
+        [ ~, metrics2 ] = evaluate_model_adaptive( imagesSeg, param );
+        metrics_search(i, j, 1) = metrics2(1);
+        metrics_search(i, j, 2) = metrics2(2);
+        metrics_search(i, j, 3) = metrics2(3);
+    end
+end
+name_file = strcat(directory_write, 'Grid_search/grid_alpha_rho.mat' );
+save(name_file, 'metrics_search', 'alphas', 'rhos')
 %%
 plot( shiftdim(metrics_search(1, 1, :)),shiftdim(metrics_search(1, 2, :)), 'r')
 %, alphas, shiftdim(metrics_search(1, 2,:)), 'g', alphas, shiftdim(metrics_search(1, 3,:)), 'b');
