3d271f4d1929c4ca848bfbd7ee088346b05bfe4a
Lidia Talavera <lidia_tama@hotmail.com>
2017-01-10 04:31:13 +0100

    task3

--- Week 02/Evaluation.m
+++ Week 02/Evaluation.m
@@ -1,4 +1,4 @@
-function [TP_images,FP_images,FN_images,TN_images] = Evaluation(Sequence,path_results,directory_imagesIn,dirIn,directory_imagesGT,dirGT,filename,video) 
+function [TP_images,FP_images,FN_images,TN_images] = Evaluation(Sequence,directory_imagesIn,dirIn,directory_imagesGT,dirGT,filename,video) 
        
 TP_images = zeros(length(Sequence), 1);
 FP_images = zeros(length(Sequence), 1);
@@ -6,7 +6,7 @@ FN_images = zeros(length(Sequence), 1);
 TN_images = zeros(length(Sequence), 1);
 
     for i=1:length(dirIn)
-        imwrite(Sequence(:,:,i),[path_results 'ResultsTask3/' video '/' dirIn(i).name ]);
+%         imwrite(Sequence(:,:,i),[path_results 'ResultsTask3/' video '/' dirIn(i).name ]);
         scene = imread(strcat(directory_imagesIn,dirIn(i).name));
         imageGT = imread(strcat(directory_imagesGT,dirGT(i).name));
         [pixelTP, pixelFP, pixelFN, pixelTN] = PerformanceAccumulationPixel(Sequence(:,:,i), imageGT);
--- Week 02/week2_task32.m
+++ Week 02/week2_task32.m
@@ -14,12 +14,12 @@ ext='.jpg';
 directory_imagesGT = strcat(directory_sequence, 'groundtruth/');
 dirGT = dir([directory_imagesGT '/*.png']);
 
-path_results = '../Week 02/';
-if exist([path_results 'ResultsTask3']) == 0
-	mkdir([path_results 'ResultsTask3/highway'])
-    mkdir([path_results 'ResultsTask3/fall'])
-    mkdir([path_results 'ResultsTask3/traffic'])
-end
+% path_results = '../Week 02/';
+% if exist([path_results 'ResultsTask3']) == 0
+% 	mkdir([path_results 'ResultsTask3/highway'])
+%     mkdir([path_results 'ResultsTask3/fall'])
+%     mkdir([path_results 'ResultsTask3/traffic'])
+% end
 
 [dim1, dim2, dim3] = size(imread(strcat(directory_imagesIn, dirIn(1).name)));
 imagesSeg = zeros(dim1, dim2, length(dirIn));
@@ -28,31 +28,50 @@ if strcmp(directory_sequence,'../Database/Week02/highway/')
     T1 = 1050;
     T2 = 1350;
     video='highway';
+    
+    K = 5
+    Rho = 0.0725
+    Threshold = 5.5
+    THFG = 0.642
 elseif strcmp(directory_sequence,'../Database/Week02/fall/')
     T1 = 1460;
     T2 = 1560;
     video='fall';
+    
+    K = 5;
+    Rho = 0.07568;
+    Threshold = 4.5;
+    THFG = 0.568;
 else
     T1 = 950;
     T2 = 1050;
     video='traffic';
+    
+    K = 4;
+    Rho = 1.0000e-05;
+    Threshold = 7.05;
+    THFG = 0.7386;
+    % 0.4307    0.6570    0.5203
+   
+%     K = 4;
+%     Rho = 1.0000e-05;
+%     Threshold = 8.0500;
+%     THFG = 0.7386;
+%     metrics2 = 0.5274    0.5950    0.5591;
+
 end
 
-K = 3;
-Rho = 0.000001;
-% Rho = 0.0000000000000000000000000007; % if tend to 0 -> long memory %0.05
-Threshold = 2000; %220 
-THFG = 0.9758; %0.85
+
 
 filename = 'Video_highway.gif';
 
 [Sequence] = MultG_fun(Threshold,T1,T2,K,Rho,THFG,video);
     
-[TP_images,FP_images,FN_images,TN_images] = Evaluation(Sequence,path_results,directory_imagesIn,dirIn,directory_imagesGT,dirGT,filename,video);
+[TP_images,FP_images,FN_images,TN_images] = Evaluation(Sequence,directory_imagesIn,dirIn,directory_imagesGT,dirGT,filename,video);
 
 [pixelTP,pixelFP,pixelFN,pixelTN,pixelPrecision, pixelRecall,pixelFMeasure] = PerformanceEvaluationPixel(TP_images, FP_images, FN_images, TN_images);
 metrics = [pixelTP,pixelFP,pixelFN,pixelTN];
-metrics2 = [pixelPrecision, pixelRecall,pixelFMeasure];
+metrics2 = [pixelPrecision, pixelRecall,pixelFMeasure]
 
 %Input :
 % Threshold : Threshold to assign pixels to a Gaussian model 
